15d269d323ccc5b53ae7eea0f0413eb092a985ac
LekoLody <96385791+LekoLody@users.noreply.github.com>
2021-12-25 02:44:23 +0400

    Govno ne lyudi

--- 6_4/src/Main.java
+++ 6_4/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Basket basket = new Basket();
+        Basket sukaNahooy = new Basket();
+        sukaNahooy.add("Rabotai", 333);
+        basket.add("Milk", 40);
+        basket.add("Koprotoshke", 33);
+        basket.print("Milk");
+        System.out.println(Basket.getAveragePrice());
+        System.out.println(Basket.wholeTotalPrice());
+        System.out.println(Basket.getProductCount());
     }
 }
