67261b361da7382846be6c634da3f285a0e9b285
LekoLody <96385791+LekoLody@users.noreply.github.com>
2022-01-07 12:24:49 +0400

    done i think

---
+++ 8_10/src/Main.java
@@ -0,0 +1,45 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int j;
+        int currentYear = LocalDate.now().getYear();
+        int currentMonth = LocalDate.now().getMonthValue();
+        int currentDay = LocalDate.now().getDayOfMonth();
+        Scanner in = new Scanner(System.in);
+        NumberAndDays numberAndDays = new NumberAndDays();
+        System.out.println("Введите год, месяц и день:");
+        numberAndDays.collectBirthdays(in.nextInt(), in.nextInt(), in.nextInt());
+        for (j = 0; j <= 0; j += 0) {
+            if (numberAndDays.year > currentYear) {
+                System.out.println("Неверный год, введите дату снова:");
+                numberAndDays.collectBirthdays(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+            else if (numberAndDays.year == currentYear &
+                    numberAndDays.day == currentDay &
+                    numberAndDays.month > currentMonth) {
+                System.out.println("Неверный месяц, введите дату снова:");
+                numberAndDays.collectBirthdays(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+            else if (numberAndDays.year == currentYear &
+                    numberAndDays.month == currentMonth &
+                    numberAndDays.day > currentDay) {
+                System.out.println("Неверный день, введите дату снова:");
+                numberAndDays.collectBirthdays(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+            else if(numberAndDays.year == currentYear &
+            numberAndDays.month > currentMonth ||
+            numberAndDays.day > currentDay){
+                System.out.println(("Неверная дата, введите дату снова"));
+            numberAndDays.collectBirthdays(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+            else System.exit(0);
+
+            }
+        }
+    }
+
