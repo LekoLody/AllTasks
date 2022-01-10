98b43735a617d6b268f7e972d78d52af88f77589
LekoLody <96385791+LekoLody@users.noreply.github.com>
2022-01-10 19:15:44 +0400

    done i think

--- 8_10_1/src/JavaBirthday.java
+++ 8_10_1/src/JavaBirthday.java
@@ -9,7 +9,7 @@ public class JavaBirthday {
         long birthdayYears = birthday.until(now, ChronoUnit.YEARS);
         long birthdayMonth = birthday.until(now, ChronoUnit.MONTHS) % 12;
         long birthdayDay = (birthday.until(now, ChronoUnit.DAYS) -
-                (birthday.until(now, ChronoUnit.YEARS) * 365 + birthdayYears / 4) - 1) % 365;
+                (birthday.until(now, ChronoUnit.YEARS) * 365 + birthdayYears / 4)) % 365;
         System.out.println(birthdayYears + " years " +
                 birthdayMonth + " months " +
                 birthdayDay + " days");
--- 8_10_1/src/Main.java
+++ 8_10_1/src/Main.java
@@ -4,6 +4,6 @@ import java.time.LocalDateTime;
 public class Main {
     public static void main(String[] args) {
         JavaBirthday javaBirthday = new JavaBirthday();
-        System.out.println(javaBirthday.getPeriodFromBirthday(LocalDate.of(1996, 1,8)));
+        System.out.println(javaBirthday.getPeriodFromBirthday(LocalDate.of(1999, 1,10)));
     }
 }
